8f37e5befabb9a2c755b7009db12a12bcb343cae
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-05-17 10:49:12 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,2 +1,8 @@
 #lang info
 (define collection 'multi)
+(define deps '("base"
+               "data-lib"))
+(define build-deps '("data-doc"
+                     "racket-doc"
+                     "rackunit-lib"
+                     "scribble-lib"))
